66081417043bb99a27ebcb95277365968568280e
Kazoroo <kazoroo1903@gmail.com>
2025-01-18 20:44:22 +0100

    Add empty inventory text to InventoryScreen when such case

--- app/src/main/java/pl/kazoroo/tavernFarkle/shop/presentation/inventory/InventoryScreen.kt
+++ app/src/main/java/pl/kazoroo/tavernFarkle/shop/presentation/inventory/InventoryScreen.kt
@@ -3,13 +3,20 @@ package pl.kazoroo.tavernFarkle.shop.presentation.inventory
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import pl.kazoroo.tavernFarkle.R
 import pl.kazoroo.tavernFarkle.core.data.local.repository.SpecialDiceList.specialDiceList
 import pl.kazoroo.tavernFarkle.shop.domain.model.SpecialDice
@@ -31,19 +38,31 @@ fun InventoryScreen(inventoryViewModel: InventoryViewModel) {
             modifier = Modifier.fillMaxSize()
         )
 
-        LazyColumn {
-            items(ownedSpecialDiceList.size) { index ->
-                val ownedDice = ownedSpecialDiceList[index]
-                val tempDice: SpecialDice = specialDiceList.find { it.name == ownedDice.name }!!
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            if(ownedSpecialDiceList.isEmpty()) {
+                item {
+                    Text(
+                        text = stringResource(R.string.your_inventory_is_empty_buy_some_special_dice_in_shop),
+                        color = Color.White,
+                        modifier = Modifier.align(Alignment.Center).width((LocalConfiguration.current.screenWidthDp / 2).dp),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            } else {
+                items(ownedSpecialDiceList.size) { index ->
+                    val specialDiceData: SpecialDice = specialDiceList.find { it.name == ownedSpecialDiceList[index].name }!!
 
-                SpecialDiceCard(
-                    isInventoryCard = true,
-                    name = ownedDice.name,
-                    image = tempDice.image,
-                    chancesOfDrawingValue = tempDice.chancesOfDrawingValue,
-                    price = tempDice.price
-                ) {
-                    //TODO: Select a special dice
+                    SpecialDiceCard(
+                        isInventoryCard = true,
+                        name = specialDiceData.name,
+                        image = specialDiceData.image,
+                        chancesOfDrawingValue = specialDiceData.chancesOfDrawingValue,
+                        price = specialDiceData.price
+                    ) {
+                        //TODO: Select a special dice
+                    }
                 }
             }
         }
