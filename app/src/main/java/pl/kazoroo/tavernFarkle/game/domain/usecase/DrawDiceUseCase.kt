727d6dd57d267fbcf6480f919962eea0c8a671a3
Kazoroo <kazoroo1903@gmail.com>
2025-01-25 11:58:36 +0100

    Refactor DrawDiceUseCase and improve readability.

--- app/src/main/java/pl/kazoroo/tavernFarkle/game/domain/usecase/DrawDiceUseCase.kt
+++ app/src/main/java/pl/kazoroo/tavernFarkle/game/domain/usecase/DrawDiceUseCase.kt
@@ -4,28 +4,46 @@ import pl.kazoroo.tavernFarkle.R
 import pl.kazoroo.tavernFarkle.core.data.local.repository.SpecialDiceList
 import pl.kazoroo.tavernFarkle.game.domain.model.Dice
 import pl.kazoroo.tavernFarkle.shop.data.model.OwnedSpecialDice
+import pl.kazoroo.tavernFarkle.shop.domain.model.SpecialDiceName
 import kotlin.random.Random
 
 class DrawDiceUseCase {
+    private val diceDrawables = listOf(
+        R.drawable.dice_1,
+        R.drawable.dice_2,
+        R.drawable.dice_3,
+        R.drawable.dice_4,
+        R.drawable.dice_5,
+        R.drawable.dice_6
+    )
+
     operator fun invoke(ownedSpecialDices: List<OwnedSpecialDice>): List<Dice> {
-        val diceDrawables = listOf(
-            R.drawable.dice_1,
-            R.drawable.dice_2,
-            R.drawable.dice_3,
-            R.drawable.dice_4,
-            R.drawable.dice_5,
-            R.drawable.dice_6
-        )
-        val isSelectedNameList = ownedSpecialDices.flatMap { dice ->
+        val selectedDiceNames = ownedSpecialDices.flatMap { dice ->
             dice.isSelected.mapIndexedNotNull { _, isSelected ->
                 if (isSelected) dice.name else null
             }
         }
+
+        //TODO: handle opponents special dice -> draw random amount of random special dice type and use them for drawing opponents dice
+        //FIXME: when user select special dice and roll again it shouldn't appear again
+        return constructRandomDiceListWithSpecials(selectedDiceNames).shuffled()
+    }
+
+    /**
+     * Constructs a list of 6 Dice, including special dice specified by name, and filling any remaining slots with regular dice.
+     *
+     * @param isSelectedNameList A list of SpecialDiceName representing the special dice to be included.
+     * @return A list of 6 Dice objects, containing the specified special dice and regular dice if needed.
+     */
+    private fun constructRandomDiceListWithSpecials(
+        isSelectedNameList: List<SpecialDiceName>
+    ): List<Dice> {
         val finalList: MutableList<Dice> = mutableListOf()
 
         isSelectedNameList.forEach { name ->
             val index = SpecialDiceList.specialDiceList.indexOfFirst { it.name == name }
-            val value = getRandomWithProbability(SpecialDiceList.specialDiceList[index].chancesOfDrawingValue)
+            val value =
+                getRandomWithProbability(SpecialDiceList.specialDiceList[index].chancesOfDrawingValue)
 
             finalList.add(
                 Dice(
@@ -35,7 +53,7 @@ class DrawDiceUseCase {
             )
         }
 
-        if(isSelectedNameList.count() < 6) {
+        if (isSelectedNameList.count() < 6) {
             val remainingCount = 6 - isSelectedNameList.count()
 
             repeat(remainingCount) {
@@ -49,11 +67,7 @@ class DrawDiceUseCase {
                 )
             }
         }
-
-        //TODO: break the code to the smaller functions to increase readability
-        //TODO: handle opponents special dice -> draw random amount of random special dice type and use them for drawing opponents dice
-        //FIXME: when user select special dice and roll again it shouldn't appear again
-        return finalList.shuffled()
+        return finalList
     }
 
     private fun getRandomWithProbability(probabilities: List<Float>): Int {
