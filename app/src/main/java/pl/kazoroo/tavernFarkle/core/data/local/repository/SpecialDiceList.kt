9ff772b353450fe343932ce964a1f4b72faa050d
Kazoroo <kazoroo1903@gmail.com>
2025-02-04 20:30:07 +0100

    Fix SpecialDiceCard layout for smaller device dimensions, set normal prices of special dice.

--- app/src/main/java/pl/kazoroo/tavernFarkle/core/data/local/repository/SpecialDiceList.kt
+++ app/src/main/java/pl/kazoroo/tavernFarkle/core/data/local/repository/SpecialDiceList.kt
@@ -8,13 +8,13 @@ object SpecialDiceList {
     val specialDiceList = listOf(
         SpecialDice(
             name = SpecialDiceName.ODD_DICE,
-            price = 2,
+            price = 150,
             image = listOf(R.drawable.odd_dice_1, R.drawable.odd_dice_2, R.drawable.odd_dice_3, R.drawable.odd_dice_4, R.drawable.odd_dice_5, R.drawable.odd_dice_6),
             chancesOfDrawingValue = listOf(26.7f, 6.7f, 26.7f, 6.7f, 26.7f, 6.7f),
         ),
         SpecialDice(
             name = SpecialDiceName.ALFONSES_DICE,
-            price = 5,
+            price = 400,
             image = listOf(R.drawable.alfonses_dice_1, R.drawable.alfonses_dice_2, R.drawable.alfonses_dice_3, R.drawable.alfonses_dice_4, R.drawable.alfonses_dice_5, R.drawable.alfonses_dice_6),
             chancesOfDrawingValue = listOf(38.5f, 7.7f, 7.7f, 7.7f, 15.4f, 23f),
         ),
