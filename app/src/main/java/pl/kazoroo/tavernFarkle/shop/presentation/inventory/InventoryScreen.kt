7765281364cc869cfb7823b5378d9ea9054337f3
Kazoroo <kazoroo1903@gmail.com>
2025-04-06 11:58:21 +0200

    2.2.5 (23) - Add "select dice..." advice to the top of InventoryScreen.kt.

--- app/src/main/java/pl/kazoroo/tavernFarkle/shop/presentation/inventory/InventoryScreen.kt
+++ app/src/main/java/pl/kazoroo/tavernFarkle/shop/presentation/inventory/InventoryScreen.kt
@@ -3,20 +3,31 @@ package pl.kazoroo.tavernFarkle.shop.presentation.inventory
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -55,7 +66,7 @@ fun InventoryScreen(inventoryViewModel: InventoryViewModel) {
                 .fillMaxSize()
                 .systemBarsPadding()
         ) {
-            if(ownedSpecialDiceList.isEmpty()) {
+            if (ownedSpecialDiceList.isEmpty()) {
                 item {
                     Box(
                         modifier = Modifier.fillParentMaxSize(),
@@ -72,6 +83,10 @@ fun InventoryScreen(inventoryViewModel: InventoryViewModel) {
                     }
                 }
             } else {
+                item {
+                    InventoryAdvice()
+                }
+
                 ownedSpecialDiceList.forEach { item ->
                     items(item.count) { index ->
                         val specialDiceData: SpecialDice = specialDiceList.find { item.name == it.name }!!
@@ -93,6 +108,40 @@ fun InventoryScreen(inventoryViewModel: InventoryViewModel) {
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun InventoryAdvice() {
+    Box(
+        modifier = Modifier
+            .height(100.dp)
+            .padding(horizontal = dimensionResource(R.dimen.medium_padding))
+            .clip(RoundedCornerShape(10.dp))
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.paper_texture),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+        )
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxHeight()
+        ) {
+            Text(
+                text = stringResource(R.string.select_dice_you_want_to_use_in_the_game),
+                modifier = Modifier
+                    .padding(dimensionResource(R.dimen.medium_padding)),
+                textAlign = TextAlign.Center
+            )
+
+            Icon(
+                imageVector = Icons.Default.Info,
+                contentDescription = null,
+                modifier = Modifier.size(25.dp)
+            )
         }
     }
 }
