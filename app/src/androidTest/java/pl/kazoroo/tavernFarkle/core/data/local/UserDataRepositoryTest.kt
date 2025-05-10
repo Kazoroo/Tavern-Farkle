d71ae4015f484bb518fc1fe9573d3ccd711d5265
Kazoroo <kazoroo1903@gmail.com>
2025-05-10 16:22:49 +0200

    Fix UserDataRepositoryTest.kt

--- app/src/androidTest/java/pl/kazoroo/tavernFarkle/core/data/local/UserDataRepositoryTest.kt
+++ app/src/androidTest/java/pl/kazoroo/tavernFarkle/core/data/local/UserDataRepositoryTest.kt
@@ -1,12 +1,8 @@
 package pl.kazoroo.tavernFarkle.core.data.local
 
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.PreferenceDataStoreFactory
-import androidx.datastore.preferences.core.Preferences
+import androidx.test.platform.app.InstrumentationRegistry
 import junit.framework.TestCase.assertEquals
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.advanceUntilIdle
@@ -15,10 +11,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.`when`
 import pl.kazoroo.tavernFarkle.core.data.local.repository.UserDataRepository
-import pl.kazoroo.tavernFarkle.core.data.local.repository.dataStore
 import java.io.File
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -33,15 +26,7 @@ class UserDataRepositoryTest {
     @Before
     fun setUp() {
         testDataStoreFile = temporaryFolder.newFile("user.preferences_pb")
-
-        val testDataStore: DataStore<Preferences> = PreferenceDataStoreFactory.create(
-            scope = CoroutineScope(Dispatchers.IO),
-            produceFile = { testDataStoreFile }
-        )
-
-        testContext = mock(Context::class.java)
-        `when`(testContext.dataStore).thenReturn(testDataStore)
-
+        testContext = InstrumentationRegistry.getInstrumentation().targetContext
         userDataRepository = UserDataRepository.getInstance(testContext)
     }
 
@@ -49,13 +34,13 @@ class UserDataRepositoryTest {
     fun saveNewValue() = runTest {
         val coins = "1100"
 
-        userDataRepository.saveNewValue(value = coins)
+        userDataRepository.saveValue(coins, UserDataKey.COINS)
         advanceUntilIdle()
     }
 
     @Test
     fun checkForDefaultValue() = runTest {
-        assertEquals(null, userDataRepository.userCoins.first())
+        assertEquals(null, userDataRepository.getBooleanFlow(UserDataKey.IS_FIRST_LAUNCH).first())
         advanceUntilIdle()
     }
 
@@ -63,8 +48,8 @@ class UserDataRepositoryTest {
     fun saveAndReadValue() = runTest {
         val coins = "1100"
 
-        userDataRepository.saveNewValue(value = coins)
-        assertEquals(coins, userDataRepository.userCoins.first())
+        userDataRepository.saveValue(coins, UserDataKey.COINS)
+        assertEquals(coins, userDataRepository.getStringFlow(UserDataKey.COINS).first())
         advanceUntilIdle()
     }
 }
