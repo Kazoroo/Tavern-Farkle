9776cbcdd43ab1d3c46cf2526c56264137daf917
Kazoroo <kazoroo1903@gmail.com>
2025-08-20 19:29:38 +0200

    Replace background image and remove blur effect.

--- app/src/main/java/pl/kazoroo/tavernFarkle/core/presentation/components/BackgroundImage.kt
+++ app/src/main/java/pl/kazoroo/tavernFarkle/core/presentation/components/BackgroundImage.kt
@@ -4,18 +4,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import pl.kazoroo.tavernFarkle.R
 
 @Composable
 fun BackgroundImage() {
     Image(
-        painter = painterResource(id = R.drawable.wooden_background_texture),
+        painter = painterResource(id = R.drawable.background_wooden),
         contentDescription = null,
         contentScale = ContentScale.Crop,
-        modifier = Modifier.fillMaxSize().blur(4.dp)
+        modifier = Modifier.fillMaxSize()
     )
 }
