04a8c9440cd83878c3e4b2f01d2e323658a020d2
Kazoroo <kazoroo1903@gmail.com>
2025-08-28 18:59:47 +0200

    Add opponent turn logic
    - Implement PlayOpponentTurnUseCase to handle opponent's turn, based on old GameViewModel
    - Inject PlayOpponentTurnUseCase into GameViewModelRefactor

--- app/src/main/java/pl/kazoroo/tavernFarkle/game/presentation/game/GameViewModelRefactor.kt
+++ app/src/main/java/pl/kazoroo/tavernFarkle/game/presentation/game/GameViewModelRefactor.kt
@@ -6,15 +6,18 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import pl.kazoroo.tavernFarkle.game.domain.model.GameState
 import pl.kazoroo.tavernFarkle.game.domain.repository.GameRepository
 import pl.kazoroo.tavernFarkle.game.domain.usecase.CalculatePointsUseCase
 import pl.kazoroo.tavernFarkle.game.domain.usecase.DrawDiceUseCase
+import pl.kazoroo.tavernFarkle.game.domain.usecase.PlayOpponentTurnUseCase
 
 class GameViewModelRefactor(
     private val repository: GameRepository,
     private val calculatePointsUseCase: CalculatePointsUseCase,
-    private val drawDiceUseCase: DrawDiceUseCase
+    private val drawDiceUseCase: DrawDiceUseCase,
+    private val playOpponentTurnUseCase: PlayOpponentTurnUseCase
 ): ViewModel() {
     val gameState: StateFlow<GameState> = repository.gameState
 
@@ -35,6 +38,13 @@ class GameViewModelRefactor(
         repository.resetDiceState()
         repository.changeCurrentPlayer()
         drawDiceUseCase(repository.gameState.value.players[gameState.value.getCurrentPlayerIndex()].diceSet)
+
+        if(repository.gameState.value.currentPlayerUuid != repository.myUuidState.value) {
+            viewModelScope.launch {
+                println("Opponent turn")
+                playOpponentTurnUseCase()
+            }
+        }
     }
 
     fun onScoreAndRollAgain() {
