36cfdb24b708ada68f3484cef9df4cd32853dc31
Kazoroo <kazoroo1903@gmail.com>
2025-09-10 17:38:58 +0200

    Change visuals of game and main menu buttons
    - new button background image
    - button borders
    - button drop shadow

--- app/src/main/java/pl/kazoroo/tavernFarkle/game/presentation/components/DiceButton.kt
+++ app/src/main/java/pl/kazoroo/tavernFarkle/game/presentation/components/DiceButton.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.core.EaseInOutQuad
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,12 +18,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.dropShadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.shadow.Shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import pl.kazoroo.tavernFarkle.R
 
@@ -40,7 +44,18 @@ fun DiceButton(
     Button(
         onClick = buttonInfo.onClick,
         shape = RoundedCornerShape(dimensionResource(id = R.dimen.rounded_corner)),
-        modifier = modifier,
+        modifier = modifier.border(
+            width = 1.dp,
+            color = Color.DarkGray,
+            shape = RoundedCornerShape(dimensionResource(id = R.dimen.rounded_corner)))
+            .dropShadow(
+                shape = RoundedCornerShape(dimensionResource(id = R.dimen.rounded_corner)),
+                shadow = Shadow(
+                    color = Color(0x60000000),
+                    radius = 10.dp,
+                    offset = DpOffset(10.dp, 15.dp)
+                ),
+            ),
         enabled = buttonInfo.enabled,
         colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
         contentPadding = PaddingValues(0.dp)
@@ -50,10 +65,11 @@ fun DiceButton(
             contentAlignment = Alignment.Center
         ) {
             Image(
-                painter = painterResource(id = R.drawable.paper_texture),
+                painter = painterResource(id = R.drawable.glossy_button),
                 contentDescription = stringResource(R.string.score_and_roll_again),
-                contentScale = ContentScale.Crop,
+                contentScale = ContentScale.FillBounds,
                 modifier = Modifier
+                    .fillMaxSize()
                     .clip(RoundedCornerShape(dimensionResource(id = R.dimen.rounded_corner))),
                 alpha = alphaBackground
             )
