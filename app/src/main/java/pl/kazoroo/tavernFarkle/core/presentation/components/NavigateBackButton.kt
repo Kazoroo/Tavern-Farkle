47c9a31c3ddd7d915613ff13ac1a38dff3cfbcdf
Kazoroo <kazoroo1903@gmail.com>
2025-10-06 19:06:16 +0200

    Replace Material Icons with local drawable resources and update dependencies
    - Increment targetSdk to 36
    - Update other dependencies
    - Add arrow_back_24dp_white.xml and settings_24dp_white.xml
    - Update Java and jvm version from 1.8 to 17

--- app/src/main/java/pl/kazoroo/tavernFarkle/core/presentation/components/NavigateBackButton.kt
+++ app/src/main/java/pl/kazoroo/tavernFarkle/core/presentation/components/NavigateBackButton.kt
@@ -1,21 +1,20 @@
 package pl.kazoroo.tavernFarkle.core.presentation.components
 
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import pl.kazoroo.tavernFarkle.R
 
 @Composable
 fun NavigateBackButton(onClick: () -> Unit) {
     IconButton(onClick = onClick) {
         Icon(
-            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+            painter = painterResource(R.drawable.arrow_back_24dp_white),
             contentDescription = "Back",
             tint = Color.White,
             modifier = Modifier.size(dimensionResource(R.dimen.coin_icon_size))
