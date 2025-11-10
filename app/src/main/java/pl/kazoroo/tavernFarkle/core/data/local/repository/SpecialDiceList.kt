36f7c27fb9e907bdb74d4931d8456b22be9908ee
Kazoroo <kazoroo1903@gmail.com>
2025-11-10 15:14:50 +0100

    Adjust the price and probabilities of the special dice, and sort the list by price.

--- app/src/main/java/pl/kazoroo/tavernFarkle/core/data/local/repository/SpecialDiceList.kt
+++ app/src/main/java/pl/kazoroo/tavernFarkle/core/data/local/repository/SpecialDiceList.kt
@@ -8,15 +8,15 @@ object SpecialDiceList {
     val specialDiceList = listOf(
         SpecialDice(
             name = SpecialDiceName.ODD_DICE,
-            price = 150,
+            price = 700,
             image = listOf(R.drawable.odd_dice_1, R.drawable.odd_dice_2, R.drawable.odd_dice_3, R.drawable.odd_dice_4, R.drawable.odd_dice_5, R.drawable.odd_dice_6),
             chancesOfDrawingValue = listOf(26.7f, 6.7f, 26.7f, 6.7f, 26.7f, 6.7f),
         ),
         SpecialDice(
             name = SpecialDiceName.ALFONSES_DICE,
-            price = 450,
+            price = 1000,
             image = listOf(R.drawable.alfonses_dice_1, R.drawable.alfonses_dice_2, R.drawable.alfonses_dice_3, R.drawable.alfonses_dice_4, R.drawable.alfonses_dice_5, R.drawable.alfonses_dice_6),
-            chancesOfDrawingValue = listOf(38.5f, 7.7f, 7.7f, 7.7f, 15.4f, 23f),
+            chancesOfDrawingValue = listOf(35.5f, 8.7f, 8.7f, 8.7f, 15.4f, 23f),
         ),
         SpecialDice(
             name = SpecialDiceName.SPIDERS_DICE,
@@ -26,9 +26,9 @@ object SpecialDiceList {
         ),
         SpecialDice(
             name = SpecialDiceName.ROYAL_DICE,
-            price = 350,
+            price = 500,
             image = listOf(R.drawable.royal_dice_1, R.drawable.royal_dice_2, R.drawable.royal_dice_3, R.drawable.royal_dice_4, R.drawable.royal_dice_5, R.drawable.royal_dice_6),
             chancesOfDrawingValue = listOf(25f, 12.5f, 12.5f, 12.5f, 18.8f,	18.8f),
         ),
-    )
+    ).sortedBy { it.price }
 }
